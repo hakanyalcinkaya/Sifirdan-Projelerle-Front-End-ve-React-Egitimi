fff9d03af30e7ea57bbb49e2187cff0793279519
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-02 17:48:24 +0300

    JS: 003 Degisken Tanimlamak

---
+++ 010-JavaScript/js/003-variable.js
@@ -0,0 +1,22 @@
+let API_KEY = "OSDIFHSDSDFGISDHGOSDIHGOIHDSFGOIHREOIG4398UY3498UERFGE98YTG4IEHRFIERHYG9834Y";
+
+console.log(API_KEY)
+
+// firstName -> camelCase
+// FirstName ???
+
+
+// Degisken Isimlerinin Dogru Kullanilmasi
+let firstName = "Dogu"
+let FirstName = "JSte Kabul Gormuyor"
+let FIRST_NAME = "Bu Degisken Bilgisinin Degismeyeceyi Varsayilir.."
+FIRST_NAME = "Bilgi let ile tanimlandigi icin degistirildi"
+console.log(firstName)
+
+// Neden Ingilizce Degisken Ismi Kullaniyoruz?
+let adım = "Hakan :)" // Bunu Kullanmayin..
+console.log(adım)
+
+// Kotu Degisken Ismi Tanimlamayi Sevenler Icin Ornek :)
+let x = "Yalcinkaya"
+console.log(x)
