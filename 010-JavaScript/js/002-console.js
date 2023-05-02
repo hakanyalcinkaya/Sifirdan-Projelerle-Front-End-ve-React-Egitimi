9810427ceecf63d6744d110a28122fd8e6b0c1c0
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-02 17:27:01 +0300

    JS: console.log

---
+++ 010-JavaScript/js/002-console.js
@@ -0,0 +1,15 @@
+console.count("Count ?")
+console.log(console)
+console.count("Count ?")
+console.error("Hata Bilgisini Dikkat Cekmesi Icin Error ile Yazabiliriz")
+
+const user = {
+  firstName: "Hakan",
+  lastName: "Yalcinkaya",
+}
+console.log("log:", user)
+
+console.log("TABLE::::")
+console.table(user)
+
+console.info("Bilgi..")
