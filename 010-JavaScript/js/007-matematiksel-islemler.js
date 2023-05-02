9bad14524f9f5c0c206ce7e61e1ea984f7393c0f
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-02 19:31:53 +0300

    JS: Number ile Matematiksel Islemler

---
+++ 010-JavaScript/js/007-matematiksel-islemler.js
@@ -0,0 +1,30 @@
+let userId = 37;
+// mod alma:
+console.log(userId / 2)
+console.log("tek mi cift mi?", userId % 2)
+
+userId ++;
+console.log("tek mi cift mi?", userId % 2)
+
+const boxSize = 8;
+let items = 29;
+
+console.log("Son Kolide kac urun var:", items % boxSize)
+console.log("Son Koliye kac adet urun daha eklenmeli?", boxSize - (items % boxSize) )
+
+// us alma
+console.log(3 * 3 * 3 * 3)
+console.log(3 ** 4)
+
+// Asagi Yuvarlama
+console.log("Asagi Yuvarlama", Math.floor(3.9) )
+
+// Yukari Yuvarlama
+console.log("Yukari Yuvarlama", Math.ceil(3.9) )
+
+// Yakina Yuvarlama
+console.log("Yakina Yuvarlama 3.9",  Math.round(3.9) )
+console.log("Yakina Yuvarlama 3.6",  Math.round(3.6) )
+console.log("Yakina Yuvarlama 3.5",  Math.round(3.5) )
+console.log("Yakina Yuvarlama 3.49", Math.round(3.49) )
+console.log("Yakina Yuvarlama 3.4",  Math.round(3.4) )
