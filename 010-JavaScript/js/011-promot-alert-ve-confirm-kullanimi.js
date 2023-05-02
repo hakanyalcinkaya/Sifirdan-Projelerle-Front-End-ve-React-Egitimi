06d0597e0883e6e6bce4be040b2d8c1dcf6681f0
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-02 22:30:21 +0300

    JS: promot alert ve confirm kullanimi

---
+++ 010-JavaScript/js/011-promot-alert-ve-confirm-kullanimi.js
@@ -0,0 +1,25 @@
+// Prompt, Alert ve Confirm
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
+
+// Not: Dialog ile Model Yapilari Olusturulabilir..
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog
+
+
+// Prompt:
+
+// const info = prompt("En Sevdiginiz Yazilim Dili Hangisi", "Python")
+
+// Alert:
+// alert(info)
+
+// console.log(info)
+
+// console.log(
+//   confirm("Kullanici Silinsin mi?")
+// )
+
+// if (confirm("Kullanici Silinsin mi")) {
+//   console.log("Kullanici Sildindi..")
+// }
