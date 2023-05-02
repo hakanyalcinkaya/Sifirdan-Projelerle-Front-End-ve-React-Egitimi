2320241fd0d3cf14c9604ca6dc07f1ee3c61490e
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-02 18:57:20 +0300

    JS: number veri turleriyle calismak

---
+++ 010-JavaScript/js/006-number-veri-turu-ile-calismak.js
@@ -0,0 +1,83 @@
+let price = 100;
+
+console.log(
+  "price",
+  price
+)
+
+console.log(100 + 200)
+console.log(
+  "price",
+  price + 1000
+)
+
+console.log(
+  "price",
+  price
+)
+
+price + 1000;
+console.log(
+  "price",
+  price
+)
+
+price = 1000;
+console.log(
+  "price -> 1100 ?  ",
+  price
+)
+
+// eski bilgiye tekrar esitledim
+price = 100;
+
+price = price + 1000;
+console.log(
+  "price -> 1100 ?  ",
+  price
+)
+
+
+// eski bilgiye tekrar esitledim
+price = 100;
+price += 1000;
+
+console.log(
+  "price -> 1100 ?  ",
+  price
+)
+
+
+// arttirma ve azatlma islemleri
+let count = 0;
+
+count ++;
+count ++;
+count ++;
+count ++;
+
+count --;
+count --;
+
+console.log("count", count)
+
+count += 100
+console.log("count += 100", count)
+
+count -= 10
+console.log("count -= 10", count)
+
+count *= 2
+console.log("count *= 2", count)
+
+count /= 4
+console.log("count /= 4", count)
+
+const tax = 0.18
+console.log(price)
+const totalPrice = price + (price * tax)
+
+console.log("totalPrice", totalPrice)
+
+console.log(2 + 8 * 10, "Sonuc: 100?")
+console.log( (2 + 8) * 10, "Sonuc: 100?")
