d736014f851dc3eeb9d98b3e4c2f5dcdd3bbf94d
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-11 23:05:01 +0300

    JS: Object ile Calismak

---
+++ 010-JavaScript/js/025-Object-ile-Calismak.js
@@ -0,0 +1,62 @@
+// Object ile Calismak:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+
+// Obje Olusturmak
+const person = {
+  firstName: "Tugra",
+  lastName: "JavaScript",
+  "last login": "2023/01/01",
+  "register-date": "2022/01/01",
+  fullName: function () { // bunu kullan ;)
+    return `${this.firstName} ${this.lastName}`
+  },
+  fullNameV2: () => {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+// Bilgiye Erismek:
+console.log(person.firstName)
+console.log(person["lastName"])
+console.log(person["last login"])
+console.log(person["register-date"])
+
+// Bilgi Eklemek
+person.age = 22;
+
+person["last-time-changed-password"] = "2023/01/01"
+
+// Bilgi Degistirmek
+person.firstName = "Moca"
+person['lastName'] = "ES6+"
+
+console.log(person)
+
+console.log(person.fullName())
+console.log(person.fullNameV2())
+
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person))
+
+const product = {
+  isInStock: false,
+  price: 0,
+  tax: 1.18,
+  priceWithTax: function () {
+    return this.price * this.tax
+  }
+}
+
+const iPad = Object.create(product);
+iPad.name = "iPad Pro"
+iPad.price = 100;
+iPad.isInStock = true;
+
+
+console.log(iPad)
+console.log(iPad.priceWithTax())
