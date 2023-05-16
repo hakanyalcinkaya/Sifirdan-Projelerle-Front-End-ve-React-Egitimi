a6a64b65c19757b0790bf3a523f6571a4c795631
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-17 00:03:04 +0300

    JS: Bolum Sonu Calismasi 2: Verilerin Fetch Api ile Yuklenmesi

--- 010-JavaScript/bolum-sonu-calismasi-2-spa-hash-routing/main.js
+++ 010-JavaScript/bolum-sonu-calismasi-2-spa-hash-routing/main.js
@@ -5,18 +5,28 @@ console.log("window.location.hostname", window.location.hostname)
 
 const hashLinks = document.querySelectorAll('.hash-route-link')
 
+const app = document.querySelector('#app')
+
 const routes = {
   "": {
     title: "Ana Sayfa",
+    data: "./home.html",
+    isApi: false,
   },
   "#todos": {
-    title: "Todo List"
+    title: "Todo List",
+    data: "https://jsonplaceholder.typicode.com/todos/",
+    isApi: true,
   },
   "#about": {
-    title: "About Us"
+    title: "About Us",
+    data: "./about-us.html",
+    isApi: false
   },
   "#contact": {
-    title: "Contact"
+    title: "Contact",
+    data: "./contact.html",
+    isApi: false
   },
 
 }
@@ -24,6 +34,23 @@ const routes = {
 function checkRoute(hash = window.location.hash) {
   // console.log(routes[hash])
   document.title = routes[hash].title
+  const dataUrl = routes[hash].data
+  // console.log(dataUrl)
+  isApi = routes[hash].isApi
+  if (isApi) {
+    app.innerHTML = ""
+    const createUl = document.createElement('ul')
+    app.append(createUl)
+    fetch(dataUrl).then(res => res.json()).then(data => {
+      data.forEach(item => {
+        const createLi = document.createElement('li')
+        createLi.innerHTML = item.title
+        createUl.append(createLi)
+      })
+    })
+  } else if (!isApi) {
+    fetch(dataUrl).then(res => res.text()).then(data => app.innerHTML = data)
+  }
 }
 
 hashLinks.forEach(item => item.addEventListener("click", (event) => {
