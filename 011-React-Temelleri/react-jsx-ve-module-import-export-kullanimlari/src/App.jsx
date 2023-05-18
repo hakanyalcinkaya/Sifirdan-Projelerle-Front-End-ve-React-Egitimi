c8e871f5bba32786f90a4cf35dde88ba1f1fa940
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-18 16:10:18 +0300

    React Temelleri: JSX Kurallari

--- 011-React-Temelleri/react-jsx-ve-module-import-export-kullanimlari/src/App.jsx
+++ 011-React-Temelleri/react-jsx-ve-module-import-export-kullanimlari/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import './App.css'
 
 function App() {
-  // 1: return ederkenken HTML Yapisi Return Etmen Gerekiyor
+  // 1: return ederkenken HTML Yapisi Return Etmen Gerekiyor (JSX)
   // 2: return ile sadece bir parent element gonderebilirsin.. 
   // 2.1: fragment kullanilabilir <> .. </>
   // return (
@@ -17,15 +17,30 @@ function App() {
   // 6: return icinde aciklama icin yine susluParantez Kullanilmali {/* aciklama.. */}
   // 7: style bilgisi {js icinde {obj olcak sekilde olmalidir}} -> {{backgroundColor: "yellow", listStyle: "none", margin: 0, padding: 0}}
   // 8: map kullanirken muhakkak key bilgisini gondermemiz gerekir
+  // 9: React Componentlerinin Ilk Harfleri Her Zaman Buyuk Olmali.. BirdenFalzaKelimeIse..HerKelimeninIlkHarfiBuyukOlmali..
   const userName = "Derya"
   const users = ["Lorem", "Ipsum", "Dolor"]
 
+  const products = [
+    {id: 1,name: "MacBook Pro 14", inStock: false},
+    {id: 2,name: "MacBook Pro 13", inStock: true},
+    {id: 3,name: "iPad", inStock: false},
+    {id: 4,name: "Lenovo 14", inStock: true},
+    {id: 5,name: "Dell XPS", inStock: true},
+  ]
+
   const mapUsers = users.map((item, index) => <li key={index}>{item}</li>)
 
-  return <>
+  const isActive = false
+
+  const styles = {
+    h2Style: {backgroundColor: "blue", fontSize: "24px"}
+  }
+
+  return (<>
     <div className="bg-red">Div icindeki Bilgi</div>
-    <h1>Merhaba (2 + 2 String Bilgi) {2 + 2} {userName}</h1>
-    <h2>Kullanicilarin Sayisi: {users.length}</h2>
+    <h1 style={{backgroundColor: `${isActive ? "green" : "orange"}`}}>Merhaba (2 + 2 String Bilgi) {2 + 2} {userName}</h1>
+    <h2 style={styles.h2Style}>Kullanicilarin Sayisi: {users.length}</h2>
     <img src="https://picsum.photos/200/300" alt="" />
     <div className="bg-red">BG Red Class ile Eklendi</div>
     {/* Bu Kullanim Dogru Degil */}
@@ -43,7 +58,22 @@ function App() {
     <ul style={{backgroundColor: "yellow", listStyle: "none", margin: 0, padding: 0}}>
       {mapUsers}
     </ul>
-  </>
+
+    <ul>
+      {products
+        .filter(item => item.inStock)
+        .map(item => 
+          <li key={item.id}>{item.name.toUpperCase()}</li>
+        )
+      }
+    </ul>
+    <ImgThumbnail />
+  </>)
+}
+
+// Yeni Component
+function ImgThumbnail() {
+  return <img src="https://picsum.photos/600/250" alt="" style={{padding: "4px", borderRadius: "8px", border: "1px solid #bdc3c7"}} />
 }
 
 export default App
