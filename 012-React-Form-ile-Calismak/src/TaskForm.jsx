4a38ac8dabb9d860b0a388d19f7f788e7b7b42bd
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-05-20 19:40:29 +0300

    React: Form ile Calismak - Form-dan gelen bilgi task yapisina eklendi

--- 012-React-Form-ile-Calismak/src/TaskForm.jsx
+++ 012-React-Form-ile-Calismak/src/TaskForm.jsx
@@ -1,38 +1,65 @@
-import { useRef } from "react";
+import { useState } from "react"
 
 export default function TaskForm() {
-  const form = useRef()
+  const emptyForm = {task: "", priority: false, }
+  const [formData, setFormData ] = useState(emptyForm)
+  const [tasks, setTasks] = useState([]) 
+
+  function handleInputChange(event) {
+    setFormData(prev => {
+      return {
+        ...prev,
+        [event.target.name]: event.target.type === "checkbox" ? event.target.checked : event.target.value
+      }
+    })
+    
+  }
 
   function handleFormSubmit(event) {
     event.preventDefault()
-    console.log(event.target.elements)
-    const items = Array.from(event.target.elements).filter(item => item.name)
-    
-    console.log(items[1].checked)
+    console.log(formData)
+    if (formData.task.length > 3) {
+      setTasks(
+        prev => 
+        [formData, ...prev]
+      )
+      setFormData(emptyForm)
+      event.target.reset()
+    }
   }
 
   return (
     <>
-      <form onSubmit={handleFormSubmit} ref={form}>
+    <ul>
+      {tasks.map((item, index) => <li key={index}>{item.task}</li>)}
+    </ul>
+      <form onSubmit={handleFormSubmit}>
         <div className="row mb-3">
           <label htmlFor="task" className="col-sm-2 col-form-label">Task</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" id="task" name="task" />
+            <input 
+              type="text" className="form-control" 
+              id="task" name="task" 
+              onChange={handleInputChange} 
+            />
           </div>
         </div>
-        
         
         <div className="row mb-3">
           <div className="col-sm-10 offset-sm-2">
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="priority" name="priority" />
+              <input 
+                className="form-check-input" 
+                type="checkbox" id="priority" name="priority" 
+                onChange={handleInputChange} 
+              />
                 <label className="form-check-label" htmlFor="priority">
                   Oncelikli
                 </label>
             </div>
           </div>
         </div>
-        <button type="submit" className="btn btn-primary">Sign in</button>
+        <button type="submit" className="btn btn-primary">Kaydet</button>
       </form>
     </>
   )
