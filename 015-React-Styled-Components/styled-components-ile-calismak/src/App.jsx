0c63161fb8ef631a182195de4f556339d2606a6b
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-06-28 21:45:12 +0300

    React - Styled-Components: React-Icons Kullanildi ve Animasyon Eklendi

--- 015-React-Styled-Components/styled-components-ile-calismak/src/App.jsx
+++ 015-React-Styled-Components/styled-components-ile-calismak/src/App.jsx
@@ -1,6 +1,36 @@
 import { useState } from 'react'
 import { StyledContainer } from './components/styled'
 import { Button } from './components/Button'
+import { CoolButton, StrongButton, SubmitButton } from './components/Button/Button.styled'
+import { FaCanadianMapleLeaf } from "react-icons/fa6";
+import { keyframes, styled } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  } 
+  to {
+    transform: rotate(360deg);    
+  }
+`
+
+const CanadaStyledIcon = styled(FaCanadianMapleLeaf)`
+  font-size: 40px;
+  animation: ${rotate} 3s linear infinite;
+  color: #e74c3c;
+  `
+
+const StyledImg = styled.img`
+  
+  width: 100%;
+  padding: 4px;
+  border: 1px solid #bdc3c7;
+  border-radius: 4px;
+
+  &:hover {
+    animation: ${rotate} 10s linear infinite;
+  }
+`
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +41,23 @@ function App() {
   return (
     <>
     <StyledContainer>
+      <StyledImg src="https://picsum.photos/id/1037/1200/300" />
+      <hr />
+      <CanadaStyledIcon />
       <h1>Styled Container Uygulandi ;)</h1>
-      <Button $primary className="btn">
+      <Button $primary className="btn" style={{marginBottom: "5px"}}>
         Incele
       </Button>
+      <br />
+      <StrongButton $primary>
+        Strong Button..
+      </StrongButton>
+      <br />
+      <CoolButton as="a" href="http://google.com" target="_blank">
+        Cool Button..
+      </CoolButton>
+      <br />
+      <SubmitButton $primary>Kaydet..</SubmitButton>
     </StyledContainer>
     </>
   )
--- 015-React-Styled-Components/styled-components-ile-calismak/src/components/Button/Button.styled.js
+++ 015-React-Styled-Components/styled-components-ile-calismak/src/components/Button/Button.styled.js
@@ -16,3 +16,39 @@ export const StyledButton = styled.button`
 `
 
 // const info = `${() =>  "xxx" }`
+
+
+// Extend..
+export const StrongButton = styled(StyledButton)`
+  border-width: 5px;
+`
+
+
+// https://gradientbuttons.colorion.co/
+export const CoolButton = styled(StyledButton)`
+  background-image: linear-gradient(to right, #1D976C 0%, #93F9B9  51%, #1D976C  100%);
+  margin: 10px;
+  padding: 15px 45px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;            
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  display: block;
+  text-decoration: none;
+
+  &:hover {
+    background-position: right center; /* change the direction of the change here */
+    color: #fff;
+    text-decoration: none;
+  }
+`
+
+export const SubmitButton = styled(StrongButton).attrs({
+  type: "submit"
+})`
+  font-size: 20px;
+  color: red;
+`
