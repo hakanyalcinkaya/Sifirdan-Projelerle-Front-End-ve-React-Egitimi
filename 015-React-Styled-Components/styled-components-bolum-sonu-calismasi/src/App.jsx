145d5264269b2c301093574e2509fc8539920a50
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-06-29 13:37:26 +0300

    React - Styled-Components: Bolum Sonu Calismasi Kurulumu

---
+++ 015-React-Styled-Components/styled-components-bolum-sonu-calismasi/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello React..</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+export default App
