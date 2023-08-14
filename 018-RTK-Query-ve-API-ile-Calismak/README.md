19bcb26e1981caa51e990fc71dc7c5c90ac891f5
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2023-08-14 19:30:37 +0300

    018 RTK Q: env Kullanimi Eklendi

--- 018-RTK-Query-ve-API-ile-Calismak/README.md
+++ 018-RTK-Query-ve-API-ile-Calismak/README.md
@@ -2,3 +2,5 @@
 
 ## NOT :
 .env dosyasini ana klasore (src degil..) sizin olusturmaniz gerekiyor.
+[.env Kullanimi:](https://vitejs.dev/guide/env-and-mode.html)
+
--- 018-RTK-Query-ve-API-ile-Calismak/src/App.jsx
+++ 018-RTK-Query-ve-API-ile-Calismak/src/App.jsx
@@ -8,8 +8,9 @@ import AxiosCategories from "./pages/axios/AxiosCategories"
 import RTKQueryCategories from "./pages/rtk-query/RTKQueryCategories"
 
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL
 
-  console.log(import.meta.env.VITE_API_URL)
+  console.log("API_URL", API_URL)
 
   return (
     <>
